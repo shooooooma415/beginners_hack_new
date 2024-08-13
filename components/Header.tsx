772d48c63477355136cb68bdc89a62cdd422da72
Mimimi19 <is0677re@ed.ritsumei.ac.jp>
2024-08-13 13:06:11 +0900

    アイコンとホーム追加

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useEffect, useState } from "react"
-
+import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Header() {
@@ -9,10 +9,15 @@ export default function Header() {
     <header className="p-4 border-b-2 border-gray-300 fixed w-full bg-white">
       <ul className="w-full max-w-3xl m-auto flex font-medium flex-row">
         <li className=' pr-4'>
-          <Link className="text-gray-700 hover:text-blue-700" href="/">Home</Link>
+          <Link className="text-gray-700 hover:text-blue-700" href="/">
+            home
+          </Link>
         </li>
         <li>
           <Link className="text-gray-700 hover:text-blue-700" href="/private">（プライベート）</Link>
+        </li>
+        <li>
+            <Link className="text-gray-700 hover:text-blue-700" href="/home">Home</Link>
         </li>
       </ul>
     </header>
