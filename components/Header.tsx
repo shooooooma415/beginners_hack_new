54a1a836f443004c3a51bd6599a07674e633acef
Shoma <shoma@nakanoshoubanoMacBook-Air.local>
2024-08-13 16:17:46 +0900

    表示内容を修正

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 
 export default function Header() {
   return (
-    <header className="p-4 border-b-2 border-gray-300 fixed w-full bg-white">
+    <header className="p-4 border-b-2 border-gray-300 w-full bg-white">
       <ul className="w-full max-w-3xl m-auto flex font-medium flex-row">
         <li className=' pr-4'>
           <Link className="text-gray-700 hover:text-blue-700" href="/">Home</Link>
