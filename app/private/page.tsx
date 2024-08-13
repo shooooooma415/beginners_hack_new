f8227275b17fae5920d83ccccc93da5c93d4d59a
mafyyu <morita0126m@gmail.com>
2024-08-13 12:01:42 +0900

    privateページに画像を表示

--- app/private/page.tsx
+++ app/private/page.tsx
@@ -1,12 +1,14 @@
 import Link from 'next/link'
-import  ImageApp from "../../components/imageApp";
+import ImageApp from "../../components/imageApp";
+import PrivateImageApp from '../../components/privateImageApp';
 
 export default function Index() {
   return (
     <>
       <h1 className="mb-4 pt-28 text-4xl">プライベートページ</h1>
-      <Link href="/">homeへ
+      <Link className="my-10 " href="/">homeへ
       </Link>
+      <PrivateImageApp />
     </>
   );
 }
