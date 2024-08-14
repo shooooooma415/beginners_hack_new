75182b1c7bab03c8783cb99544f2a5a14bd2c5f3
mafyyu <morita0126m@gmail.com>
2024-08-15 04:34:12 +0900

    aboutsite追加

--- app/aboutSite/page.tsx
+++ app/aboutSite/page.tsx
@@ -1,5 +1,37 @@
 import './style.css';
+import Image from "next/image";
 
-export default function aboutSite() {
-    return (<h2 className="custom-heading">このサイトについて</h2>);
+export default function AboutSite() {
+    return (
+        <>
+            <div className="header-container">
+                <h2 className="custom-heading">このサイトについて</h2>
+
+            </div>
+            <div className="tools">技術スタック</div>
+            <div className="image-row">
+                <Image
+                    className="nextjs"
+                    src="/imagefile/Nextjs.png"
+                    width={100}
+                    height={100}
+                    alt="Next.js Icon"
+                />
+                <Image
+                    className="supabase"
+                    src="/imagefile/supabase.png"
+                    width={100}
+                    height={100}
+                    alt="Supabase Icon"
+                />
+                <Image
+                    className="tailwindcss"
+                    src="/imagefile/tailwindcss copy.png"
+                    width={100}
+                    height={100}
+                    alt="Tailwind CSS Icon"
+                />
+            </div>
+        </>
+    );  
 }
