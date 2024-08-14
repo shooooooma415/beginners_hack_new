e54978c0fad519405144d3ab5bf0620f2f5975b7
unmy040915 <168172631+unmy040915@users.noreply.github.com>
2024-08-14 20:15:40 +0900

    変更

--- app/logout/page.tsx
+++ app/logout/page.tsx
@@ -1,16 +1,9 @@
 'use client';
 import { useRouter } from 'next/navigation';
-import { Button, styled } from "@mui/material";
 import './style.css';
 
 export default function LogoutCheck() {
     const router = useRouter();
-
-    const CustomButton = styled(Button)({
-        width: 150,
-        padding: '10px 20px',
-        margin: '0 10px', // ボタン同士に少し間隔を持たせる
-    });
 
     const handleLogout = () => {
         console.log('Logging out...');
@@ -24,23 +17,10 @@ export default function LogoutCheck() {
     return (
         <>
             <h1 className="mb-4 pt-28 text-4xl">Logout ?</h1>
-            <div style={{ display: 'flex', justifyContent: 'center', paddingBottom: '50%' }}>
-                <CustomButton
-                    className="custom-buttonYes"
-                    variant="contained"
-                    href="#contained-buttons"
-                    onClick={handleLogout}
-                >
-                    Yes
-                </CustomButton>
-                <CustomButton
-                    className="custom-buttonNo"
-                    variant="contained"
-                    href="#contained-buttons"
-                    onClick={backLogout}
-                >
-                    Back
-                </CustomButton>
+           
+            <div>
+                <button className="custom-buttonYes" onClick={handleLogout}>Logout</button>
+                <button className="custom-buttonNo" onClick={backLogout}>Cancel</button>
             </div>
         </>
     );
