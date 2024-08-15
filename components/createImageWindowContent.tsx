3fb18f3c1ef7ba2d8acd75275ef13e9c1ffd4770
Shoma <shoma@nakanoshoubanoMacBook-Air.local>
2024-08-15 11:04:26 +0900

    createImageWindowContent.cssによりトリミング

--- components/createImageWindowContent.tsx
+++ components/createImageWindowContent.tsx
@@ -3,7 +3,7 @@ import "./createImageWindowContent.css";
 const createImageWindowContent = (imageUrl: string) => {
     return `
         <div class="custom-info">
-            <div class="custom-info-item coordinates">
+            <div class="infoWindow">
                 <img src="${imageUrl}" class="img-trimming" alt="Image"/>
             </div>
         </div>`;
