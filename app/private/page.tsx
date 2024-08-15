46b1ab458d36ac91d67dbca9d95f04a09f8b0a61
unmy040915 <168172631+unmy040915@users.noreply.github.com>
2024-08-15 23:38:04 +0900

    変更

--- app/private/page.tsx
+++ app/private/page.tsx
@@ -11,21 +11,6 @@ export default function Index() {
     <>
       <h2 className = "custom-heading">プロフィール</h2>
 
-      <Image
-        src="/imagefile/FreeIcon.png"
-        width={80}
-        height={80}
-        style={{ objectFit: 'contain' }}
-        alt="Free Icon"
-        className="round-image"
-      />
-
-
-      <Link href="/editProfile" passHref>
-        <Button className="custom-button" variant="contained" disableElevation>
-          プロフィールを編集
-        </Button>
-      </Link>
 
       <div className="scroll-container">
         <PrivateImageApp />
