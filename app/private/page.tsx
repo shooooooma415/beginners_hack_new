f8412e02ac0b3afbaf5b04bc81d906623fb01c94
unmy040915 <168172631+unmy040915@users.noreply.github.com>
2024-08-16 01:31:41 +0900

    変更

--- app/private/page.tsx
+++ app/private/page.tsx
@@ -1,17 +1,11 @@
 import PrivateImageApp from '../../components/privateImageApp';
-import { Button } from "@mui/material";
-import Link from 'next/link';
 import './style.css'; // スタイルシートをインポート
-import Image from 'next/image';
-
 
 
 export default function Index() {
   return (
     <>
-      <h2 className = "custom-heading">プロフィール</h2>
-
-
+      <h2 className = "custom-heading">ライブラリ</h2>
       <div className="scroll-container">
         <PrivateImageApp />
       </div>
