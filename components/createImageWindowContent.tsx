b87fae3051b20cb640b05d18542bf46e3b202ac0
Shoma <shoma@nakanoshoubanoMacBook-Air.local>
2024-08-16 00:59:52 +0900

    ページを行き来できる

--- components/createImageWindowContent.tsx
+++ components/createImageWindowContent.tsx
@@ -1,10 +1,12 @@
 import "./createImageWindowContent.css";
 
-const createImageWindowContent = (imageUrl: string) => {
+const createImageWindowContent = (imageUrl: string, image_name: string) => {
     return `
         <div class="custom-info">
             <div class="infoWindow">
+            <a href = "/detail/${image_name}">
                 <img src="${imageUrl}" class="img-trimming" alt="Image"/>
+                </a>
             </div>
         </div>`;
 };
