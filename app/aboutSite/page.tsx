fda0f2317f739455c773f50d0ae891a59845d279
Shoma <shoma@nakanoshoubanoMacBook-Air.local>
2024-08-16 13:00:49 +0900

    ゆうたのミスをもう一回変更ミスをもう一回変更させもう一回変更させられた

--- app/aboutSite/page.tsx
+++ app/aboutSite/page.tsx
@@ -1,7 +1,6 @@
 import './style.css';
 import Image from "next/image";
 import Link from 'next/link'
-
 export default function AboutSite() {
     return (
         <>
@@ -10,68 +9,81 @@ export default function AboutSite() {
             </div>
             <div className="teamname">作成チーム</div>
             <div className="detailname">りんごかじり虫</div>
-            <Link href = "https://www.instagram.com/___n.s415/profilecard/?igsh=MW5pazB6d3dyMmhiaQ==">Shoooooooma</Link>
-            <Link href = "https://www.instagram.com/un_my_04915/profilecard/?igsh=MTY2dTB1OW5zbTVrdg==">ゆった</Link>
-            <Link href = "https://www.instagram.com/mz7042406?igsh=dTQ3cmJiMHNqbTZ4&utm_source=qr">ホズミ</Link>
-            <Link href = "https://www.instagram.com/ru1y_o?igsh=MW0wOXhra3J2enRvaQ%3D%3D&utm_source=qr">もりた まさたか</Link>
+            <Link href="https://www.instagram.com/___n.s415/profilecard/?igsh=MW5pazB6d3dyMmhiaQ==">Shoooooooma</Link>
+            <Link href="https://www.instagram.com/un_my_04915/profilecard/?igsh=MTY2dTB1OW5zbTVrdg==">ゆった</Link>
+            <Link href="https://www.instagram.com/mz7042406?igsh=dTQ3cmJiMHNqbTZ4&utm_source=qr">ホズミ</Link>
+            <Link href="https://www.instagram.com/ru1y_o?igsh=MW0wOXhra3J2enRvaQ%3D%3D&utm_source=qr">もりた まさたか</Link>
             <div className="tools">技術スタック</div>
             <div className="image-row">
-                <Image
-                    className="nextjs"
-                    src="/imagefile/Nextjs.png"
-                    width={100}
-                    height={100}
-                    alt="Next.js Icon"
-                />
-                <Image
-                    className="supabase"
-                    src="/imagefile/supabase.png"
-                    width={100}
-                    height={100}
-                    alt="Supabase Icon"
-                    href =
-                />
-                <Image
-                    className="tailwindcss"
-                    src="/imagefile/tailwind.png"
-                    width={100}
-                    height={100}
-                    alt="Tailwind CSS Icon"
-                    href =
-                />
-                <Image
-                    className="github"
-                    src="/imagefile/github copy.png"
-                    width={100}
-                    height={100}
-                    alt="github Icon"
-                    href =
-                />
-                <Image
-                    className="vercel"
-                    src="/imagefile/vercel.png"
-                    width={100}
-                    height={100}
-                    alt="vercel Icon"
-                    href =
-                />
-                <Image
-                    className="react"
-                    src="/imagefile/React copy.png"
-                    width={100}
-                    height={100}
-                    alt="react Icon"
-                    href =
-                />
-                <Image
-                    className="googlemapapi"
-                    src="/imagefile/googlemap copy.png"
-                    width={100}
-                    height={100}
-                    alt="google map api Icon"
-                    href =
-                />
+                <Link href="https://nextjs.org/">
+                    <Image
+                        className="nextjs"
+                        src="/imagefile/Nextjs.png"
+                        width={100}
+                        height={100}
+                        alt="Next.js Icon"
+                    />
+                </Link>
+                <Link href="https://supabase.com/">
+                    <Image
+                        className="supabase"
+                        src="/imagefile/supabase.png"
+                        width={100}
+                        height={100}
+                        alt="Supabase Icon"
+                    />
+                </Link>
+                <Link href="https://tailwindcss.com/">
+                    <Image
+                        className="tailwindcss"
+                        src="/imagefile/tailwind.png"
+                        width={100}
+                        height={100}
+                        alt="Tailwind CSS Icon"
+
+                    />
+                </Link>
+                <Link href="https://github.co.jp/">
+                    <Image
+                        className="github"
+                        src="/imagefile/github copy.png"
+                        width={100}
+                        height={100}
+                        alt="github Icon"
+
+                    />
+                </Link>
+                <Link href="https://vercel.com/docs">
+                    <Image
+                        className="vercel"
+                        src="/imagefile/vercel.png"
+                        width={100}
+                        height={100}
+                        alt="vercel Icon"
+
+                    />
+                </Link>
+                <Link href="https://ja.react.dev/">
+                    <Image
+                        className="react"
+                        src="/imagefile/React copy.png"
+                        width={100}
+                        height={100}
+                        alt="react Icon"
+
+                    />
+                </Link>
+                <Link href="https://developers.google.com/maps/documentation/javascript?hl=ja">
+                    <Image
+                        className="googlemapapi"
+                        src="/imagefile/googlemap copy.png"
+                        width={100}
+                        height={100}
+                        alt="google map api Icon"
+
+                    />
+                </Link>
             </div>
         </>
-    );  
+    );
 }
